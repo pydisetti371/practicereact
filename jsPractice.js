04d6efb402e6bddd2881bb30b943001edc2a0d88
PYDISETTI REVANTH <31175001+pydisetti371@users.noreply.github.com>
2023-12-19 19:38:53 +0530

    counting duplicate values with data structures ES6

--- jsPractice.js
+++ jsPractice.js
@@ -14,7 +14,22 @@ function calculateDuplicates(str){
   }
   
   // console.log(calculateDuplicates('madhan'));
-  
+
+let string = 'ancnvnhjrenhrejnjjermklnjklp';
+let letters =  new Map();
+console.log(letters);
+for (let i=0; i<string.length; i++) {
+    let letter = string[i];
+//      console.log(letter)
+  if (!letters.has(letter)) {
+      letters.set(letter,1)
+  } else {
+    console.log(letter,",",letters.get(letter),"<-letter")
+      letters.set(letter,letters.get(letter)+1)
+  }
+}
+
+console.log(letters)
    
   function removeDuplicates(str) {
    let removeDuplicate = ''; 
